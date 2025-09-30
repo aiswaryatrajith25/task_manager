f4d376e6711c93da0c1f1d772073c38085ee165a
aiswaryatrajith25 <aiswaryatrajith25@gmail.com>
2025-09-30 17:19:30 +0400

    Table Migrations Completed

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('is_admin',['Yes','No'])->default('No');
             $table->rememberToken();
             $table->timestamps();
         });
